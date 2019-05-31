45340dfed556d158574cbfcba2c787ca92f2b8e8
William M <wmooney1984@gmail.com>
2019-05-30 22:03:36 -0400

    MVP Completed

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,80 @@
 // CODE here for your Lambda Classes
+
+class Person {
+  constructor(attrs) {
+    this.name = attrs.name;
+    this.age = attrs.age;
+    this.location = attrs.location;
+  }
+
+  speak() {
+    console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+  }
+}
+
+class Instructor extends Person {
+  constructor(iAttrs) {
+    super(iAttrs);
+    this.specialty = iAttrs.specialty;
+    this.favLanguage = iAttrs.favLanguage;
+    this.catchPhrase = iAttrs.catchPhrase;
+  }
+
+  demo(subject) {
+    console.log(`Today we are learning about ${subject}`);
+  }
+
+  grade(student, subject) {
+    console.log(`${student.name} receives a perfect score on ${subject}`);
+  }
+}
+
+class Student extends Person {
+  constructor(sAttrs) {
+    super(sAttrs);
+    this.previousBackground = sAttrs.previousBackground;
+    this.className = sAttrs.className;
+    this.favSubject = sAttrs.favSubject;
+  }
+
+  listsSubjects() {
+    this.favSubject.forEach(element => {
+      console.log(element);
+    });
+  }
+
+  PRAssignment(subject) {
+    console.log(`${this.name} has submitted a PR for ${subject}`);
+  }
+
+  springChallenge(subject) {
+    console.log(`${this.name} has begun spring challenge on ${subject}`);
+  }
+}
+
+class ProjectManager extends Instructor {
+  constructor(pmAttrs) {
+    super(pmAttrs);
+    this.gradClassName = pmAttrs.gradClassName;
+    this.favInstructor = pmAttrs.favInstructor;
+  }
+
+  standup(channel) {
+    console.log(`${this.name} announces to ${channel}, @channel standy times!`);
+  }
+
+  debugsCode(student, subject) {
+    console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
+  }
+}
+
+const fred = new Instructor({
+  name: 'Fred',
+  location: 'Bedrock',
+  age: 37,
+  favLanguage: 'JavaScript',
+  specialty: 'Front-end',
+  catchPhrase: `Don't forget the homies`
+});
+
+console.log(fred);
